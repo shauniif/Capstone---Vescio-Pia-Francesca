c8c2cadb7f1e1a7496e66c608fd7bc17496e96df
shauniif <francescavescio12@gmail.com>
2024-08-26 10:18:28 +0200

    -

---
+++ Capstone --- Vescio Pia Francesca (BE)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Capstone_____Vescio_Pia_Francesca__BE_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
